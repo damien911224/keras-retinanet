3d563b58e084d1c145ad9720aeb10250896e2e98
damien911224 <damien911224@gmail.com>
2019-08-23 17:46:23 +0900

    revise filter_detections to return overall scores

--- keras_retinanet/layers/filter_detections.py
+++ keras_retinanet/layers/filter_detections.py
@@ -99,7 +99,8 @@ def filter_detections(
     pad_size = keras.backend.maximum(0, max_detections - keras.backend.shape(scores)[0])
     boxes    = backend.pad(boxes, [[0, pad_size], [0, 0]], constant_values=-1)
     scores   = backend.pad(scores, [[0, pad_size]], constant_values=-1)
-    overall_scores = backend.pad(overall_scores, [[0, 0, pad_size]], constant_values=-1)
+    # (M, C)
+    overall_scores = backend.pad(overall_scores, [[0, pad_size], [0, 0]], constant_values=-1)
     labels   = backend.pad(labels, [[0, pad_size]], constant_values=-1)
     labels   = keras.backend.cast(labels, 'int32')
     other_   = [backend.pad(o, [[0, pad_size]] + [[0, 0] for _ in range(1, len(o.shape))], constant_values=-1) for o in other_]
